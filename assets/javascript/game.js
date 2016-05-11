308053adefe3f12d5847d0d85b2fbb1102e09d52
gracelyndurst <gracelyndurst@yahoo.com>
2016-05-11 13:56:08 -0500

    styled

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,7 @@ function reset(){
 	guessedWrongLetters = new Array();
 	guessedLetters = new Array();
 	//Sets blanks for the array of correctly chosen letters
-	for(i=0; i<currentWord.length; i++){/*text+=currentWord[i];*/ guessedCorrectLetters[i]=("_");}
+	for(i=0; i<currentWord.length; i++){ guessedCorrectLetters[i]=("_");}
 	//Sets the guessCounter to 5 more than the currentWord	
 	guessCounter = (currentWord.length + 5);
 }
@@ -28,7 +28,7 @@ var imgChoices = ["assets/images/maudies.png", "assets/images/chuys.gif", "asset
 var chosenString = wordChoices[Math.floor(Math.random() * wordChoices.length)];
 var index=wordChoices.indexOf(chosenString);
 var chosenImage=imgChoices[index];
-	console.log(imgChoices[index]);
+	//console.log(imgChoices[index]);
 //Changes chosen string into an array
 var currentWord=chosenString.split("");
 var guessedCorrectLetters = new Array(currentWord.length);
@@ -39,23 +39,36 @@ var guessedLetters = new Array();
 	//Sets the guessCounter to 5 more than the currentWord	
 	var guessCounter = (currentWord.length + 5);
 	
-			var d = currentWord.join("");
-			console.log(d);
+			//var d = currentWord.join("");
+			//console.log(d);
 
 	//waits for keyup event to trigger the start of the game
 	document.onkeyup = function(event) {var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
+		if((userGuess=='1') || (userGuess == '2') || (userGuess == '3') ||
+				(userGuess == '4') || (userGuess == '5') || (userGuess == '6') ||
+				(userGuess == '7') || (userGuess == '8') || (userGuess == '9') || 
+				(userGuess == '0')){return false;}
+		//Makes sure user types valid letter and then if letter chosen already
+		if ((userGuess == 'a') || (userGuess == 'b') || (userGuess == 'c') || 
+			(userGuess == 'd') || (userGuess == 'e') || (userGuess == 'f') || 
+			(userGuess == 'g') || (userGuess == 'h') || (userGuess == 'i') || 
+			(userGuess == 'j') || (userGuess == 'k') || (userGuess == 'l') || 
+			(userGuess == 'm') || (userGuess == 'n') || (userGuess == 'o') || 
+			(userGuess == 'p') || (userGuess == 'q') || (userGuess == 'r') || 
+			(userGuess == 's') || (userGuess == 't') || (userGuess == 'u') || 
+			(userGuess == 'v') || (userGuess == 'w') || (userGuess == 'x') || 
+			(userGuess == 'y') || (userGuess == 'z')){
 
-		//Makes sure user types valid letter
-		if ((userGuess == 'a') || (userGuess == 'b') || (userGuess == 'c') || (userGuess == 'd') || (userGuess == 'e') || (userGuess == 'f') || (userGuess == 'g') || (userGuess == 'h') || (userGuess == 'i') || (userGuess == 'j') || (userGuess == 'k') || (userGuess == 'l') || (userGuess == 'm') || (userGuess == 'n') || (userGuess == 'o') || (userGuess == 'p') || (userGuess == 'q') || (userGuess == 'r') || (userGuess == 's') || (userGuess == 't') || (userGuess == 'u') || (userGuess == 'v') || (userGuess == 'w') || (userGuess == 'x') || (userGuess == 'y') || (userGuess == 'z')){//checks to see if letter chosen already
 				for(i=0; i<guessedLetters.length; i++){
 					if(userGuess===guessedLetters[i]){return false;}
 				}
+
 			}
 
 			//Iterate thru array comparing userguess to current word
 			//rplc matching letter in guessedCorrectLetters array
-			for(i=0; i<currentWord.length; i++){				 /*text+=currentWord[i];*/ 						
+			for(i=0; i<currentWord.length; i++){				  						
 				if (userGuess===currentWord[i]){
 					guessedCorrectLetters[i]=userGuess;
 				}
@@ -66,17 +79,17 @@ var guessedLetters = new Array();
 					guessedWrongLetters.push(temp);
 			//putting wrong guess to beginning of guessedLetters
 					guessedLetters.unshift(userGuess);
-					console.log(guessedLetters[0]);
+					//console.log(guessedLetters[0]);
 
 
 			//converts arrays to strings then compares
 			//if they match increment wins
 			var a = currentWord.join(" ");
-			console.log(a);
+			//console.log(a);
 			var b = guessedCorrectLetters.join(" ");
-			console.log(b);
+			//console.log(b);
 			var c = guessedWrongLetters.join(" ");
-			console.log(c);
+			//console.log(c);
 			if(a===b){
 				wins++;
 				var winImage=chosenImage;
@@ -86,20 +99,28 @@ var guessedLetters = new Array();
 
 	//decreases the guess counter by 1
 	guessCounter=(guessCounter-1);
+	//checks to see if number of guesses has been exhausted
+		if(guessCounter<1){
+			wins=0;
+			reset();
+			document.getElementById("image1").src ="assets/images/food_plate.jpg";
+
+		}
 	
-			var html = //"<p>Press any key to get started.</p>" +
-			"<p>Wins : " + 
+			var html = "<h4>Choose a letter.</h4>" +
+			"<p></p>" +
+			"<h4>Wins : " + 
 			wins + 
-			"</p>" +
-			"<p>Current Word : " + 
+			"</h4>" +
+			"<h4>Current Word : " + 
 			b + 
-			"</p>" +
-			"<p>Number of Guesses Remaining : " + 
+			"</h4>" +
+			"<h4>Number of Guesses Remaining : " + 
 			guessCounter + 
-			"</p>" +
-			"<p>Letters Already Guessed : " + 
+			"</h4>" +
+			"<h4>Letters Already Guessed : " + 
 			 c + 
-			"</p>";
+			"</h4>";
 			//Placing the html into the game console
 			document.querySelector('#game_console').innerHTML=html;
 
